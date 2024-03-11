61085b6c0820fe02f52868487506ea8e2b2acf09
Aazib Chaudhry <aazibch47@gmail.com>
2024-03-11 16:58:17 +0500

    Remove login button on auth pages

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,25 +1,20 @@
 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
-// import classes from './main-header.module.css';
-// import NavLink from './nav-link';
-// import config from '@/app/api/auth/[...nextauth]/config';
-// import { redirect } from 'next/navigation';
-// import LogoutButton from './logout-button';
-import { useState } from 'react';
 import HeaderLogo from './header-logo';
 import HeaderUserMenu from './header-user-menu';
 import HeaderNavigation from './header-navigation';
 
 export default function MainHeader() {
+  const path = usePathname();
+
   return (
     <>
       <nav className="border-gray-200 bg-green-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
           <HeaderLogo />
-          <HeaderUserMenu />
+          {!path.startsWith('/auth/') && <HeaderUserMenu />}
           <HeaderNavigation />
         </div>
       </nav>
