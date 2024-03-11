0956756426afe5099f4611fb4e9b2817580b723b
Aazib Chaudhry <aazibch47@gmail.com>
2024-03-11 13:38:54 +0500

    Setup floating UI

--- components/main-header/header-user-menu.js
+++ components/main-header/header-user-menu.js
@@ -1,19 +1,45 @@
 'use client';
 
 import { useState } from 'react';
+import {
+  useFloating,
+  offset,
+  flip,
+  shift,
+  autoUpdate,
+  useDismiss,
+  useRole,
+  useInteractions,
+  FloatingFocusManager,
+  useClick
+} from '@floating-ui/react';
 
 export default function HeaderUserMenu() {
-  const [showUserMenu, setShowUserMenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleUserMenu = () => {
-    setShowUserMenu((prevShowUserMenu) => !prevShowUserMenu);
-  };
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    middleware: [offset(5), flip(), shift()],
+    whileElementsMounted: autoUpdate
+  });
+
+  const click = useClick(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    click,
+    dismiss,
+    role
+  ]);
 
   return (
     // User button
-    <div className="flex items-center md:order-2 ml-auto md:ml-8 md:mr-0 mr-2 relative">
+    <div className="flex items-center md:order-2 ml-auto md:ml-8 md:mr-0 mr-2">
       <button
-        onClick={toggleUserMenu}
+        ref={refs.setReference}
+        {...getReferenceProps()}
         type="button"
         className="flex text-sm bg-gray-700 rounded-full md:me-0 focus:ring-2 focus:ring-gray-700"
         id="user-menu-button"
@@ -29,26 +55,31 @@ export default function HeaderUserMenu() {
         />
       </button>
       {/* <!-- User dropdown menu --> */}
-      <div
-        className={`${
-          showUserMenu ? '' : 'hidden'
-        } z-50 absolute top-10 right-[-0.25rem] w-40 text-base list-none divide-y rounded-lg shadow bg-gray-700 divide-gray-600`}
-        id="user-dropdown"
-      >
-        <div className="px-4 py-3">
-          <span className="block text-sm text-white">John Doe</span>
-          <span className="block text-sm truncate text-gray-400">
-            john@domain.com
-          </span>
-        </div>
-        <ul className="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">
-              Sign out
-            </button>
-          </li>
-        </ul>
-      </div>
+      {isOpen && (
+        <FloatingFocusManager context={context} modal={false}>
+          <div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            {...getFloatingProps()}
+            className="z-50 text-base list-none divide-y rounded-lg shadow bg-gray-700 divide-gray-600"
+            id="user-dropdown"
+          >
+            <div className="px-4 py-3">
+              <span className="block text-sm text-white">John Doe</span>
+              <span className="block text-sm truncate text-gray-400">
+                john@domain.com
+              </span>
+            </div>
+            <ul className="py-2" aria-labelledby="user-menu-button">
+              <li>
+                <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-600 text-gray-200 hover:text-white">
+                  Sign out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </FloatingFocusManager>
+      )}
     </div>
   );
 }
