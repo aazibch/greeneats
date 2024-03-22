58148a562dffa0d6ee08445cf4ef319914673836
Aazib Chaudhry <aazibch47@gmail.com>
2024-03-23 00:35:06 +0500

    Fix Container styling

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,6 +1,6 @@
 export default function Container({ children, className }) {
   return (
-    <div className={`container mx-auto max-w-[90rem] ${className}`}>
+    <div className={`container mx-auto max-w-[90rem] px-4 ${className}`}>
       {children}
     </div>
   );
