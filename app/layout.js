118e60dbc5b8dc545d751fe2b6e35f4dc29b967f
Aazib Chaudhry <aazibch47@gmail.com>
2024-03-24 09:05:28 +0500

    Edit globals.css

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
 import './globals.css';
 import AuthProvider from '@/components/authentication/auth-provider';
 import Layout from '@/components/layout/layout';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   title: 'GreenEats - For the conscientious eater',
   description: 'Vegan and vegetarian dishes from all over the world.'
 };
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <AuthProvider>
           <Layout>{children}</Layout>
